907bcc84c76c29fdb70495446065e15dda510155
Ghosh <debmalya.ghosh@capgemini.com>
2023-06-02 23:36:26 +0530

    initial commit

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ app.listen(process.env.PORT,()=>
 {
 console.log(`Server is wroking on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 })
+
+/*
+git init
+git add .
+git commit -m "initial commit"
+git status
+*/
